92ce195e65367cc0dfa74c3f851fb4f77d5b6408
muhammad-abdusalam <muhammad.adam4078@gmail.com>
2023-05-14 11:44:50 +0300

    added loading event

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,35 +4,42 @@ import { useLogin } from "../hooks/useLogin";
 const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const { login, error } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const user = await login(username, password);
     props.setUser(user);
+    setLoading(false);
   };
 
   return (
     <div className="Login">
       <h2>Log In</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <button>Log in</button>
-        {error && <p className="error">{error}</p>}
-      </form>
+      {!loading ? (
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <button>Log in</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+      ) : (
+        <p className="loading">Loading...</p>
+      )}
     </div>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,42 +5,49 @@ const Signup = (props) => {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const { signup, error } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const user = await signup(name, username, password);
     props.setUser(user);
+    setLoading(false);
   };
 
   return (
     <div className="signup">
       <h2>Sign Up</h2>
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <button>Sign up</button>
-        {error && <p className="error">{error}</p>}
-      </form>
+      {!loading ? (
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <button>Sign up</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+      ) : (
+        <p className="loading">Loading...</p>
+      )}
     </div>
   );
 };

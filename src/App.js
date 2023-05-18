0a749fa572baefd71e3f3696e906a0ef31cad421
muhammad-abdusalam <muhammad.adam4078@gmail.com>
2023-05-18 15:11:49 +0300

    fix performance issues

--- src/App.js
+++ src/App.js
@@ -10,29 +10,38 @@ import Axios from "axios";
 function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [tasks, setTasks] = useState(null);
+  const errorMsg = "Request is not authorized!";
 
   const todo_user = JSON.parse(localStorage.getItem("todo-user"));
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem("todo-user")));
+    getTasks();
+  }, []);
+
+  const getTasks = () => {
     if (todo_user) {
       Axios.get(`${serverUrl}/api/tasks`, {
         headers: {
           Authorization: `Bearer ${todo_user.token}`,
         },
       })
-        .then(() => {
+        .then((response) => {
           setLoading(false);
+          setTasks(response.data);
         })
-        .catch(() => {
-          setUser(null);
-          localStorage.removeItem("todo-user");
+        .catch((error) => {
           setLoading(false);
+          if (error.response.data.error === errorMsg) {
+            localStorage.removeItem("todo-user");
+            setUser(null);
+          }
         });
     } else {
       setLoading(false);
     }
-  }, []);
+  };
 
   return (
     <HashRouter>
@@ -46,7 +55,13 @@ function App() {
                   path="/"
                   element={
                     user ? (
-                      <Home user={user} setUser={setUser} />
+                      <Home
+                        user={user}
+                        setUser={setUser}
+                        getTasks={getTasks}
+                        setTasks={setTasks}
+                        tasks={tasks}
+                      />
                     ) : (
                       <Navigate to={"/signup"} />
                     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,29 @@
 import { useState } from "react";
-import { useEffect } from "react";
-import Axios from "axios";
 import TaksList from "../components/TasksList";
 import AddTaskForm from "../components/AddTaskForm";
 import EditTaskForm from "../components/EditTaskForm";
-import { serverUrl } from "../App";
-import { useNavigate } from "react-router-dom";
 
 const Home = (props) => {
-  const [tasks, setTasks] = useState(null);
   const [taskId, setTaskId] = useState("");
   const [taskTitle, setTaskTitle] = useState("");
-  const errorMsg = "Request is not authorized!";
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
-  const getTasks = () => {
-    Axios.get(`${serverUrl}/api/tasks`, {
-      headers: {
-        Authorization: `Bearer ${props.user.token}`,
-      },
-    })
-      .then((response) => {
-        setTasks(response.data);
-        console.log("finish");
-      })
-      .catch((error) => {
-        if (error.response.data.error === errorMsg) {
-          localStorage.removeItem("todo-user");
-          props.setUser(null);
-          navigate("/login");
-        }
-      });
-  };
 
   return (
     <div className="home">
       <AddTaskForm
         user={props.user}
         setUser={props.setUser}
-        getTasks={getTasks}
-        tasks={tasks}
-        setTasks={setTasks}
+        getTasks={props.getTasks}
+        tasks={props.tasks}
+        setTasks={props.setTasks}
       />
       <TaksList
         user={props.user}
         setUser={props.setUser}
-        tasks={tasks}
-        setTasks={setTasks}
+        tasks={props.tasks}
+        setTasks={props.setTasks}
         setTaskId={setTaskId}
         setTaskTitle={setTaskTitle}
-        getTasks={getTasks}
+        getTasks={props.getTasks}
       />
       <EditTaskForm
         user={props.user}
@@ -61,7 +31,7 @@ const Home = (props) => {
         taskTitle={taskTitle}
         setTaskTitle={setTaskTitle}
         taskId={taskId}
-        getTasks={getTasks}
+        getTasks={props.getTasks}
       />
     </div>
   );
